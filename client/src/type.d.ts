69c0557e693535558d06c3bdd4c41ebac1c8d873
qozha <qozha@pop-os.localdomain>
2021-04-22 15:07:53 +0600

    api done

---
+++ client/src/type.d.ts
@@ -0,0 +1,38 @@
+interface IQuiz {
+    id: string
+    quizname: string
+    questions: IQuestion[]
+}
+
+interface IAttempt {
+    id: number
+    quizID: number
+    submittedAnswers: number[]
+    score: number
+}
+
+interface IQuestion {
+    id: string
+    question: string
+    answers: string[]
+    correctAnswer: string
+}
+
+type ApiDataTypeQ = {
+    message: string
+    status: string
+    quizzes: IQuiz[]
+    quiz?: IQuiz
+}
+
+type ApiDataTypeQSingle = {
+    message: string
+    status: string
+    quiz: IQuiz
+}
+
+type ApiDataTypeAttempt = {
+    message: string
+    status: string
+    attempt: IAttempt
+}
